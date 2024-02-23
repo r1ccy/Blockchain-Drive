d882f06a0be0e9a29ff87716ccad9087ae342ecf
r1ccy <128091346+r1ccy@users.noreply.github.com>
2024-02-23 13:56:10 -0300

    APP and Constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,6 +13,6 @@ export const contractAbi = abi;
 // ! ||                                   Pinata IPFS                                  ||
 // ! ||--------------------------------------------------------------------------------||
 
-export const API_Key =  '412cbfdb57d0e1ffc1fe'
-export const API_Secret=  '7kEX5-H0lJcU9zzpCA6NnCTeyHJZaK9ftAElt56SPU1Pa2KcYxksSzMHqKBwMwUD'
-export const JWT= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI5ZDI3YjAyOC04MGJiLTRhMzAtOWI3Ny0xNDYzZjkzODFlYTkiLCJlbWFpbCI6InIxY2N5QG91dGxvb2suY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjQxMmNiZmRiNTdkMGUxZmZjMWZlIiwic2NvcGVkS2V5U2VjcmV0IjoiMGJkM2I5MTkzYTYyMjRhZTgyOWJhMDc3NzgwZTQxYTQ5MzMyNjZhYTRiMTU0MTE1MjQ5YzY0OTlmNmM3NzczMSIsImlhdCI6MTcwODY5NzA4OH0.jKpR488JHx3_zghpL85aosmUR7IyGBh2A3N0WTWbI_4'
+export const API_Key =  '92d92da1ac2c3647f5a0'
+export const API_Secret=  '70c876f1144f0020a61d6bfdfb9aedae75939e361eb02b33bd8b761d2f7032cd'
+export const JWT= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI5ZDI3YjAyOC04MGJiLTRhMzAtOWI3Ny0xNDYzZjkzODFlYTkiLCJlbWFpbCI6InIxY2N5QG91dGxvb2suY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjkyZDkyZGExYWMyYzM2NDdmNWEwIiwic2NvcGVkS2V5U2VjcmV0IjoiNzBjODc2ZjExNDRmMDAyMGE2MWQ2YmZkZmI5YWVkYWU3NTkzOWUzNjFlYjAyYjMzYmQ4Yjc2MWQyZjcwMzJjZCIsImlhdCI6MTcwODcwNzMxN30.ya1L-u7MkbiRS_1anFuh2upoaBWW2FDc1kVZthhrxm0'
